19cd0c12659cc3777ded29dabe95a9d1e6048ac7
Andreas Draganis <andreas.draganis@gmail.com>
2021-07-21 00:04:59 +0100

    Added fps control and timing.

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,13 @@ extern crate opengl_graphics;
 use glutin_window::GlutinWindow;
 use piston::WindowSettings;
 
-use piston::event_loop::{EventSettings, Events};
+use piston::event_loop::{EventLoop, EventSettings, Events};
 use piston::input::{Button, ButtonState, Key};
 use piston::{ButtonEvent, RenderEvent};
 
 use opengl_graphics::{GlGraphics, OpenGL};
+
+use std::time::Instant;
 
 mod ring;
 
@@ -33,21 +35,35 @@ fn main() {
     let mut window: GlutinWindow = settings.build().expect("Could not create window");
     let mut gl = GlGraphics::new(opengl);
 
+    let frames_per_second = 10;
+    let time_per_frame = 1.0/frames_per_second as f32;
+    let mut previous_now = Instant::now();
+    let mut now = Instant::now();
+
     let mut events = Events::new(EventSettings::new());
+    events.set_max_fps(frames_per_second);
     let mut player = ring::Ring::new(0.0, 0.0, 50.0, RED);
+    let rr = ring::Ring::new((WINDOW_SIZE as f64)*0.35, (WINDOW_SIZE as f64)*0.35, (WINDOW_SIZE as f64)*0.25, YELLOW);
+
     while let Some(event) = events.next(&mut window) {
         if let Some(render_args) = event.render_args() {
             gl.draw(render_args.viewport(), |context, gl_graphics| {
-                graphics::clear(BLACK, gl_graphics);
+                
+                previous_now = now;
+                now = Instant::now();
+                let time_elapsed = now - previous_now;
+                println!("tick!! {}", time_elapsed.as_secs_f32().to_string());
 
-                let rr = ring::Ring::new((WINDOW_SIZE as f64)*0.35, (WINDOW_SIZE as f64)*0.35, (WINDOW_SIZE as f64)*0.25, YELLOW);
+                //update(time_elapsed)
+                graphics::clear(BLACK, gl_graphics);
                 rr.draw(&context.draw_state,
                         context.transform,
                         gl_graphics);
-
                 player.draw(&context.draw_state,
                             context.transform,
                             gl_graphics);
+
+                
             });
         }
         if let Some(k) = event.button_args() {
