86bc5e71a5865739b6c65ea324abc59267bec4fa
Andreas Draganis <andreas.draganis@gmail.com>
2021-07-25 22:18:04 +0100

    Moved game loop code into Game struct.

--- src/game.rs
+++ src/game.rs
@@ -1,16 +1,56 @@
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston_window;
+
 use super::constants;
 use super::renderer;
-use super::scene::*;
+use super::scene;
 use piston_window::*;
+use std::time::Instant;
+
+const WINDOW_SIZE: u32 = 512;
 
 pub struct Game {
-    scene: Scene,
+    scene: scene::Scene,
 }
 
 impl Game {
     pub fn new() -> Self {
-        let scene = Scene::new();
+        let scene = scene::Scene::new();
         Game { scene }
+    }
+
+    pub fn run(&self) {
+        let mut window: PistonWindow =
+            piston_window::WindowSettings::new("Osmos clone", (WINDOW_SIZE, WINDOW_SIZE))
+                .exit_on_esc(true)
+                .build()
+                .unwrap();
+
+        let frames_per_second = 10;
+        let updates_per_second = 10;
+        let mut previous_now = Instant::now();
+        let mut now = Instant::now();
+
+        let mut game = Game::new();
+
+        window.set_max_fps(frames_per_second);
+        window.set_ups(updates_per_second);
+
+        while let Some(event) = window.next() {
+            if let Some(_) = event.render_args() {
+                previous_now = now;
+                now = Instant::now();
+                let time_elapsed = now - previous_now;
+                println!("tick!! {}", time_elapsed.as_secs_f32().to_string());
+
+                game.draw(&mut window, &event);
+            }
+
+            if let Some(button_args) = event.button_args() {
+                game.handle_button_args(button_args, &window);
+            }
+        }
     }
 
     pub fn draw(&mut self, window: &mut PistonWindow, event: &Event) {
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,6 @@
-extern crate piston;
-extern crate piston_window;
-
-extern crate graphics;
-extern crate opengl_graphics;
-
-//use piston_window::{Button, Key, ButtonState, WindowSettings, PistonWindow, RenderEvent, ButtonEvent};
-use piston_window::*;
-
-use std::time::Instant;
-
-use osmos_clone::game::*;
-
-const WINDOW_SIZE: u32 = 512;
+use osmos_clone::game;
 
 fn main() {
-    let mut window: PistonWindow =
-        piston_window::WindowSettings::new("Osmos clone", (WINDOW_SIZE, WINDOW_SIZE))
-            .exit_on_esc(true)
-            .build()
-            .unwrap();
-
-    let frames_per_second = 10;
-    let updates_per_second = 10;
-    let mut previous_now = Instant::now();
-    let mut now = Instant::now();
-
-    let mut game = Game::new();
-
-    window.set_max_fps(frames_per_second);
-    window.set_ups(updates_per_second);
-
-    //while let Some(event) = events.next(&mut window) {
-    while let Some(event) = window.next() {
-        if let Some(_) = event.render_args() {
-            previous_now = now;
-            now = Instant::now();
-            let time_elapsed = now - previous_now;
-            println!("tick!! {}", time_elapsed.as_secs_f32().to_string());
-
-            game.draw(&mut window, &event);
-        }
-
-        if let Some(button_args) = event.button_args() {
-            game.handle_button_args(button_args, &window);
-        }
-    }
+    let game = game::Game::new();
+    game.run();
 }
