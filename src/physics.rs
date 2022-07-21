1376e3602ed11ac00a2f21cd6c4bcb4cae7ec08c
Andreas Draganis <andreas.draganis@gmail.com>
2022-07-21 12:58:02 +0200

    Naming fix.

--- src/physics.rs
+++ src/physics.rs
@@ -1,7 +1,7 @@
 use super::scene;
 
 pub fn integrate(scene: &mut scene::Scene, time_step: f64) {
-    for enemy in scene.cell_collection.get_cells_mut() {
-        enemy.position += time_step * enemy.velocity;
+    for cell in scene.cell_collection.get_cells_mut() {
+        cell.position += time_step * cell.velocity;
     }
 }
